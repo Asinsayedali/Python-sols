db3d9aa119a0b575e51ae7f87f202f52bcbfc8ef
asinsayedali <asinsayedali008@gmail.com>
2025-05-14 18:25:05 +0530

    added new problem

---
+++ matrix.py
@@ -0,0 +1,12 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        self.matrix_values = [
+            list(map(int, row.split()))
+            for row in matrix_string.split('\n')
+        ]
+
+    def row(self, index):
+        return self.matrix_values[index - 1]
+
+    def column(self, index):
+        return [row[index - 1] for row in self.matrix_values]
